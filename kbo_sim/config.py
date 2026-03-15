e9a6ef885efa2c59e5585ee06cbb975a8488bc26
achard9999-hash <achard9999@gmail.com>
2026-03-15 23:52:08 +0900

    Update config.py

--- kbo_sim/config.py
+++ kbo_sim/config.py
@@ -11,7 +11,16 @@ TEAM_ALIASES = {
     "SSG": "SSG",
     "키움": "키움",
     "LG": "LG",
+    "WO": "키움",
+    "OB": "두산",
+    "LT": "롯데",
+    "SS": "삼성",
+    "SK": "SSG",
+    "HH": "한화",
 }
+
+TEAMS = ["KIA", "두산", "롯데", "삼성", "한화", "KT", "NC", "SSG", "키움", "LG"]
+USER_TEAM = "한화"
 
 FIELDING_CLEAN_RATE = {
     "삼성": 0.984,
@@ -26,13 +35,11 @@ FIELDING_CLEAN_RATE = {
     "키움": 0.977,
 }
 
-TEAMS = ["KIA", "두산", "롯데", "삼성", "한화", "KT", "NC", "SSG", "키움", "LG"]
-
-PITCHER_ROLE_ORDER = [
-    "선발1", "선발2", "선발3", "선발4", "선발5",
-    "추격조1", "추격조2", "추격조3", "추격조4", "추격조5",
-    "셋업맨2", "셋업맨1", "마무리"
-]
+STARTER_ROLES = ["선발1", "선발2", "선발3", "선발4", "선발5"]
+CHASE_ROLES = ["추격조1", "추격조2", "추격조3", "추격조4", "추격조5"]
+SETUP_ROLES = ["셋업맨2", "셋업맨1"]
+CLOSER_ROLE = "마무리"
+PITCHER_ROLE_ORDER = STARTER_ROLES + CHASE_ROLES + SETUP_ROLES + [CLOSER_ROLE]
 
 HIGH_LEVERAGE_BY_INNING = {
     7: "셋업맨2",
@@ -43,4 +50,6 @@ HIGH_LEVERAGE_BY_INNING = {
 LEAGUE_BASE_ONBASE = 0.340
 LEAGUE_BASE_K = 0.22
 MAX_STEAL_ATTEMPT_RATE = 0.36
-USER_TEAM = "한화"
+MAX_EXTRA_INNING = 11
+MANUAL_CHASE_CHANGES_PER_GAME = 2
+MANUAL_SETUP_CHANGES_PER_MONTH = 4
